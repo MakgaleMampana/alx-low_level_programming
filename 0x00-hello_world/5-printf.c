3d7e6580ec110603853e393152cc9c51258e236b
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-18 15:22:43 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main: is where the function start
- * returns a zero
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
