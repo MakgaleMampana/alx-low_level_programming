e24b8e06010f50667cd06aa13ee5ac7f43605591
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-18 15:18:31 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main: is where the function start
+ * returns a zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
