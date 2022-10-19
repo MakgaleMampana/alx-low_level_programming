359fabb63c89c8cb90e748f20d2a4755cfa56226
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-19 16:43:30 -0700

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: print letters in lowercase
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char lcl = 'a';
+
+for (lcl = 'a'; lcl <= 'z'; lcl++)
+{
+putchar(lcl);
+putchar('\n');
+}
+return (0);
+}
+
+
