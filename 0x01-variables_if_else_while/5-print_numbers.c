02eda3167ece0ac5f169774430b985054171773a
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-20 12:21:20 -0700

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description:print all single base 10 numbers
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+
+{
+int n = 0;
+
+for (n = 0; n <= 9; n++)
+{	
+printf("%d", n);
+
+}
+printf("\n");
+
+return (0);
+}
