66f5ad37990c448bb8c19694d7d347c9195dd1cd
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-27 11:31:38 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*add- entry point
+*
+* @i:will be the first interger
+* @j:the second intergert
+* @n:will be the sum of i and j
+*
+*description: 'take two intergers and add them together'
+*
+*Return:n,the sum of the 2 intergers
+*/
+
+int add(int i,int j)
+{
+	int n;
+
+	n = (i + j);
+	return (n);
+
+}
+
+
