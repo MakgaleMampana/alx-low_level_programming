188503fb626d84d88569d82d9080d7da152bf819
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-27 11:42:36 -0700

    add 2 int

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,19 +5,16 @@
 *
 * @i:will be the first interger
 * @j:the second intergert
-* @n:will be the sum of i and j
 *
 *description: 'take two intergers and add them together'
 *
-*Return:n,the sum of the 2 intergers
+*Return:the sum of the 2 intergers
 */
 
-int add(int i,int j)
+int add(int i, int j)
 {
-	int n;
 
-	n = (i + j);
-	return (n);
+	return ((i + j));
 
 }
 
