89bc60a231a60ffae75e30f005ee4b45781bd9bb
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-26 19:41:11 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*_abs- entry point
+*
+*@n: input variable
+*description: 'returns abs value of n'
+*
+*Return: the absulute value of n
+*/
+
+int _abs(int n)
+{
+	i int;
+
+	if (n < 0)
+	{
+		i = (n * -1);
+		return (i);
+
+	}
+	else
+	{
+		return (n);
+	}
+}
+
