04338b8e740d5179bd0ccff5553c15728c91b072
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-26 19:58:43 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_last_digit- entry point
+*
+*@n: input variable
+*description: 'print last digit of n'
+*
+*Return:the value of the last digit
+*/
+
+int print_last_digits(int n)
+{
+	int ld;
+	if (n < 0)
+	{
+		n = (n * -1);
+		ld = (n % 10);
+	}
+	else
+	{
+		ld = (n % 10);
+	}
+
+	_putchar(ld);
+	return (ld);
+}
+
