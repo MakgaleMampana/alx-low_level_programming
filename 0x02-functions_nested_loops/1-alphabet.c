a7e1bf5b0535bda7945c04bf094aa288f1840d40
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-26 18:07:43 -0700

    x

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-*print_alphabet : the function will print letters in lowercase
+*print_alphabet- entry point
 *
-*description : lowercase letters printed
+*description: 'lowercase letters printed'
 *
-*return : always 0 (sucess)
+*return: always 0 (succes)
 */
 
 void print_alphabet(void)
