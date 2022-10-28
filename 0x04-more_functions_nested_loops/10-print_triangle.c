98d68b37b96d69c946109c006711b1648947bb2e
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-28 01:22:34 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+*print_triangle- entry point
+*
+*@size: input variable
+*
+*description: 'print a triangle with #'
+*
+*return:# in a shape of triangle
+*/
+
+void print_triangle(int size)
+{
+	int count = 1;
+	int count_2 = 1;
+
+	if (size > 0)
+	{
+		while (count <= size)
+		{
+			for (count_2 = 1; count_2 < size; count_2++)
+			{
+				_putchar(' ');
+			}
+			for (count_2 = 1; count_2 <= count; count_2++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+			count++;
+
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+
+}
+
