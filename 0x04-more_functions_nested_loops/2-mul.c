fb40e1a5565a026deb28479b821ef759af337a4f
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-27 19:29:52 -0700

    multiply 2 int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*mul- entry point
+*
+* @i:will be the first interger
+* @j:the second intergert
+*
+*description: 'take two intergers and multiply them together'
+*
+*Return:the product of the 2 intergers
+*/
+
+int add(int i, int j)
+{
+
+	return ((i * j));
+
+}
+
+
