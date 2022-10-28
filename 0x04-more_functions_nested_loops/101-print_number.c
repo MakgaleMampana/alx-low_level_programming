7f3a4141c78093483ae0fde4cf2500819ab65ecc
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-28 03:23:53 -0700

    print interger

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*print_number- entry point
+*
+*@n: input variable
+*
+*description: 'prints an integer'
+*
+*Return:an interger
+*/
+
+void print_number(int n)
+{
+	if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+
+
+}
+
