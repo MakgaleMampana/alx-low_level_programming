25a77e0a332f6e8e2388115e4183de70544b96c7
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-28 11:05:33 -0700

    elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,13 +17,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= (n - 1); i++)
 	{
-		if (i != (n - 1)) 
-		{
-			printf("%d, ",  a[i]);
-		}
+		printf("%d, ",  a[i]);
+
 	}
-	printf("%d", a[n - 1]);
-	
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+
 	printf("\n");
 
 }
