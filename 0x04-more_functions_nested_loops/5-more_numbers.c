75b27f20644feb88e160ce973e746cdda9a3ab84
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-27 20:16:02 -0700

    numbers(0-14) 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*more_numbers- entry point
+*
+*description: 'numbers(0-14) printed 10 times'
+*
+*return:numbers 0-14, 10 times
+*/
+
+void more_numbers(void)
+{
+	int i = 0;
+	int l = 1;
+
+	while (l <= 10)
+	{
+
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+
+		}
+		l++;
+		_putchar('\n');
+	}
+
+}
+
