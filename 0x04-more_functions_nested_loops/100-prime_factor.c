6319aab92492b68b3b59a1b606b9683b91122ab0
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-28 02:36:07 -0700

    max prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -19,14 +19,14 @@ int main(void)
 	while (n % 2 == 0)
 	{
 		max_pf = 2;
-		n = n/2;
+		n = n / 2;
 	}
-	for (i = 3; i <= sqrt(n); i = i +2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
 			max_pf = i;
-			n = n/i;
+			n = n / i;
 		}
 	}
 	if (n > 2)
