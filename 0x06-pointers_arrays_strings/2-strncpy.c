018b2b627c9c2628bc9a584d7837989a630d4039
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-10-28 20:20:45 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,16 +16,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (i < n && *(src + i) != '\0')
 	{
-		if (i < n)
-		{
-			dest[i] = src[i];
-		}
-		while (i < n)
-		{
-			dest[i++] = '\0';
-		}
+		*(dest + i) = *(src + i);
+		i++;
 	}
+	while (i != n)
+	{
+		dest[i++] = '\0';
+	}
+
 	return (dest);
 }
