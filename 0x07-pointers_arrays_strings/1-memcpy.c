e90b57940ef606a2210eacc6b7078f3e80856418
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-11-06 11:59:47 -0800

    commit tjis

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _memset- copies n bytes from memory area src to memory area dest
+ * _memset- Entry point
  *
- *  @dest: input variable
- *  @src: input variable 2
- *  @n: input variable 3
+ * Description: copies n bytes from memory area src to memory area dest
+ * @dest: input variable
+ * @src: input variable 2
+ * @n: input variable 3
  *
- *  Return: a pointer to dest
- *  */
+ * Return: a pointer to dest
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
