f38fee82ab9331bedadc57ee5f99635883f40fdb
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-11-06 11:40:02 -0800

    even more pointers and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr- locates a character in a string.
+ *
+ *  @s: input variable
+ *  @c: input variable 2
+ *
+ *  Return: a pointer to the first occurrence of the characte
+ *  */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+
+}
+
