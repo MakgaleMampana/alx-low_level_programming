ff6ced8f4008241a126f4a2950ae2e769f5b5d6a
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-11-15 14:57:36 -0800

    arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,21 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-       	int j = 1;
+int i;
+int j = 1;
 
-	if (argc != 3)
-	{
-		printf("%s\n", "Error");
-		return (1);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			j *= atoi(argv[i]);
-		}
-		printf("%d\n", j);
-	}
-	return (0);
+if (argc != 3)
+{
+printf("%s\n", "Error");
+return (1);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+j *= atoi(argv[i]);
+}
+printf("%d\n", j);
+}
+return (0);
 }
