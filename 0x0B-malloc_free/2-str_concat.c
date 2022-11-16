7b446f038daaedaadc54ff1fc8a8624449ec4c1d
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-11-16 14:11:04 -0800

    allocation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat- entry point
+ * Description: Function
+ * @s1: Input variable
+ * @s2: Input variable 2
+ * Return: pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+
