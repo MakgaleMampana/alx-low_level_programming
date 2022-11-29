92b0ee264e36aaee0820b53cbd4321469e61e56a
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-11-29 14:43:26 -0800

    mak commits

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * listint_len- entry
+ * @h: input
+ * Return: l
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *ptr;
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	ptr = h;
+	while (ptr != NULL)
+	{
+		i++;
+		ptr = ptr->next;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint- entry
+ * @head: input
+ * Return: l
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *h, *i;
+
+	h = head;
+	if (head != NULL)
+	{
+		while (h != NULL)
+		{
+			i = h;
+			h = h->next;
+			free(i);
+		}
+		free(h);
+	}
+}
