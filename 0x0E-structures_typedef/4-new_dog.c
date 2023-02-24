d7e0b04234ee364388c031976e5a51245224681c
MakgaleMampana <makgale.mampana94+git@gmail.com>
2023-02-24 15:05:19 -0800

    ll

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,88 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
-int get_len(int i);
-char *str_cpy(char *dest, char *src);
+#include <stdlib.h>
+
 /**
- * new_dog - a function that creates a new dog
- * get len of name + owner, malloc them, cpy name + owner to new
- * @name: name
- * @age: age
- * @owner: owner
- * Return: 0
+ *_strlen - prints the size of a string in bytes
+ *
+ *@s: pointer that will store the string's location
+ *
+ *Return: size of the string without the null \0 placed at the end of it
+ */
+int _strlen(char *s)
+{
+	int num;
+
+	num = 0;
+	while (*s != 0)
+	{
+		num++;
+		s++;
+	}
+	return (num);
+}
+
+/**
+ **_strcpy - copies the string from a pointer to another
+ *
+ *@src: pointer that has the string information
+ *@dest: pointer to be copied
+ *
+ *Return: string copied to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *src != 0; dest++, src++, i++)
+	{
+		*dest = *src;
+	}
+	for (*dest = 0; i > 0; dest--, src--, i--)
+	{
+	}
+	return (dest);
+}
+
+/**
+ *new_dog - creates a new dog
+ *
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ *
+ *Return: the variable already initialized
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_name;
-	char *copy_name, *copy_owner;
-	unsigned int x, name_len = 0, owner_len = 0;
+	dog_t *new_dog;
+	char *new_name, *new_owner;
 
-	new_name = malloc(sizeof(dog_t));
-	if (name == NULL)
-		return (NULL);
-	if (name == NULL || age <= 0 || owner == NULL)
+	if (name == NULL || owner == NULL)
 	{
-		free(new_name);
 		return (NULL);
 	}
-
-	for (x = 0; name[x] != '\0'; x++)
-		name_len++;
-
-	for (x = 0; owner[x] != '\0'; x++)
-		owner_len++;
-
-	copy_name = malloc(sizeof(char) * (name_len + 1));
-	if (copy_name == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
 		return (NULL);
-
-	copy_owner = malloc(sizeof(char) * (owner_len + 1));
-	if (copy_owner == NULL)
+	}
+	new_name = malloc(sizeof(char) + _strlen(name) + 1);
+	if (new_name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-
-	for (x = 0; x <= name_len; x++)
-		copy_name[x] = name[x];
-
-	for (x = 0; x <= owner_len; x++)
-		copy_owner[x] = owner[x];
-
-	new_name->name = copy_name;
-	new_name->owner = copy_owner;
-	new_name->age = age;
-	return (new_name);
+	}
+	new_owner = malloc(sizeof(char) + _strlen(owner) + 1);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_name, name);
+	_strcpy(new_owner, owner);
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = new_owner;
+	return (new_dog);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,21 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
+
 /**
- * free_dog - Entry Point
- * @d: struct to free
- * Return: 0
+ *free_dog - frees dogs
+ *
+ *@d: struct to be freed
+ *
+ *Return: nothing
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d);
+	dog_t *fdog = d;
+
+	if (d != 0)
+	{
+		free(fdog->name);
+		free(fdog->owner);
+		free(fdog);
+	}
 }
