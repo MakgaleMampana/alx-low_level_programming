bc6d91909503d8c1e64a8c263fcfba29ca934df1
MakgaleMampana <makgale.mampana94+git@gmail.com>
2023-02-24 14:21:46 -0800

    lsl

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,36 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-void print_error(void) {
-  printf("Error\n");
-  exit(98);
-}
-
-int parse_integer(const char* str) {
-  int result = 0;
-  for (int i = 0; str[i] != '\0'; i++) {
-    if (!isdigit(str[i])) {
-      print_error();
-    }
-    result = result * 10 + (str[i] - '0');
-  }
-  return result;
-}
-
-void multiply(const char* str1, const char* str2) {
+/**
+ * multiply - multiplies two positive numbers and prints the result
+ * @str1: the first number
+ * @str2: the second number
+ */
+void multiply(const char* str1, const char* str2)
+{
   int num1 = parse_integer(str1);
   int num2 = parse_integer(str2);
   int result = num1 * num2;
-  printf("%d\n", result);
+
+  char buffer[20];
+  sprintf(buffer, "%d\n", result);
+  for (size_t i = 0; i < strlen(buffer); i++) {
+    _putchar(buffer[i]);
+  }
 }
 
-int main(int argc, char** argv) {
-  if (argc != 3) {
-    print_error();
-  }
-  multiply(argv[1], argv[2]);
-  return 0;
-}
